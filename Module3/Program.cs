68a47dfa466efe0852de6a30430abf603bdefc80
Krechet <b3zn06nm@gmail.com>
2025-09-27 13:32:33 +0300

    FavDay

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -4,19 +4,18 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        DaysOfWeek MyFavoriteDay;
 
-        Console.WriteLine("Your name?");
-        string name = Console.ReadLine();
-        Console.WriteLine("Your age?");
-        byte age = byte.Parse(Console.ReadLine());
-        Console.WriteLine("Have a pet?");
-        bool pet = bool.Parse(Console.ReadLine());
-        Console.WriteLine("Foot size?");
-        double size = double.Parse(Console.ReadLine());
+        MyFavoriteDay = DaysOfWeek.Friday;
 
-        Console.WriteLine("My name is {0} \n My age is {1}, \n Do I have a pet? {2}; \n My foot size {3}", name, age, pet, size);
-
-        Console.ReadKey();
-
+        Console.WriteLine(MyFavoriteDay);
     }
+}
+
+enum DaysOfWeek : byte
+{
+    Tuesday,
+    Monday,
+    Wednesday,
+    Friday
 }
