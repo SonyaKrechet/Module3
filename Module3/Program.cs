60deccfef769823f4c4f95eb72c660355f409d61
Krechet <b3zn06nm@gmail.com>
2025-09-27 14:55:39 +0300

    enum var and date

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -11,9 +11,9 @@ class MainClass
     public static void Main(string[] args)
 
     {
-
+        Console.WriteLine("Enter your name");
         var name = Console.ReadLine();
-
+        Console.WriteLine("Enter your age");
         var age = checked((byte)int.Parse(Console.ReadLine()));
         Console.WriteLine("Your name is {0} and age is {1} ", name, age);
 
@@ -21,6 +21,10 @@ class MainClass
 
         var day = (DayOfWeek)int.Parse(Console.ReadLine());
         Console.WriteLine("Your favorite day is {0}", day);
+        Console.WriteLine("Enter your birthday");
+        var birthday = DateOnly.Parse(Console.ReadLine());
+        Console.WriteLine("Your birthday date is {0}", birthday);
+
 
     }
 
