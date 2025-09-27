a088c504282ecc3326f3d81290f76f2d066db62d
Krechet <b3zn06nm@gmail.com>
2025-09-27 14:43:38 +0300

    enum и parse

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Xml.Linq;
+enum DayOfWeeks
+{
+    Monday = 1,
+    Tuesday = 2
+}
 
 class MainClass
 {
     public static void Main(string[] args)
-    {
-        string name = "Евгения";
-        string age = "27";
-        string favcolor = "black";
-        Console.WriteLine($"{name} \n {age} \n {favcolor}");
+    
+        {
+      
+
+      
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your age");
+        int age = checked((int.Parse(Console.ReadLine())));
+
+        Console.WriteLine("Your name is {0} and age is {1}", name, age);
+        Console.WriteLine("What is your favourite day of week");
+        byte day = checked((byte.Parse(Console.ReadLine())));
+        Console.WriteLine("Your favourite day is {0}", (DayOfWeeks) day);
 
         Console.ReadKey();
-    }
+        }
+
+
+    
 }
