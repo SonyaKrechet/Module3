e6fac290847580615c1f3ecab02201a0e145564e
Krechet <b3zn06nm@gmail.com>
2025-10-05 11:10:56 +0300

    columns

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -10,17 +10,13 @@ class Program
             { 10, 11, 12 },
             {13, 15, 17 }
         };
-         for (int i = 0; i <= numbers.GetUpperBound(0) + 1; i++)
-        { for (int k = 0; k <= numbers.GetUpperBound(1); k++)
-            { Console.Write(numbers[i, k] + " "); }
+         for (int i = 0; i < numbers.GetUpperBound(1) + 1; i++)
+        { for (int k = 0; k < numbers.GetUpperBound(0) + 1; k++)
+            { Console.Write(numbers[k, i] + " "); }
             Console.WriteLine();
 }
 
 
-
-
-
-        Console.ReadKey();
     }
     
         }
