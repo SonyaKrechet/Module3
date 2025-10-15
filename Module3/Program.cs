58e99900c4299e7543fd1a9c80ee82a2bb87b1a7
Krechet <b3zn06nm@gmail.com>
2025-10-15 08:03:16 +0300

    Задание 5.2.3

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -1,28 +1,46 @@
 ﻿using System;
-    class Program
-{
-    static void Main(string[] args)
-    {
-        
-        int[] result = GetArrayFromConsole();
-        
-        foreach (int number in result)
-        {
-            Console.WriteLine(number);
-        }
-    }
-    static int[] GetArrayFromConsole()
-    {
-       var result = new int[5];
+using System.Drawing;
+using System.Net.NetworkInformation;
 
-       for (int i = 0; i < result.Length; i++)
-       {
-           Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-           result[i] = int.Parse(Console.ReadLine());
-                
-        }
-        Array.Sort(result);
-        return result;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
         
+        var user = Profile();
+        string[] colors = ShowColor(user.name);
+        
+        Console.WriteLine($"You profile is: {user.name}, {user.age}");
+        foreach (string color in colors)
+        {
+            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color, true);
+            Console.WriteLine($"Ваш любимый цвет: {color}");
+        }
     }
+
+    static (string name, int age) Profile()
+    {
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+        Console.Write("Введите возраст с цифрами: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        var user = (name, age);
+        return user;
+    }
+    static string[] ShowColor(string name)
+    {
+        
+        string[] colors = new string[3];
+
+        int i = 0;
+        while (i < colors.Length)
+        {
+            Console.Write($"{name} Введите любимый цвет #{i + 1}: ");
+            colors[i] = Console.ReadLine();
+            i++;
+        }
+        return colors;
+    }
+
+    
 }
