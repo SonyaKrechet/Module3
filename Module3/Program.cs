5df0faa7b3b71ee8209c6b350ee3e63690877ead
Krechet <b3zn06nm@gmail.com>
2025-10-25 11:42:20 +0300

    методы и сортировка массива

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -1,46 +1,28 @@
 ﻿using System;
-using System.Drawing;
-using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        
-        var user = Profile();
-        string[] colors = ShowColor(user.name);
-        
-        Console.WriteLine($"You profile is: {user.name}, {user.age}");
-        foreach (string color in colors)
-        {
-            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color, true);
-            Console.WriteLine($"Ваш любимый цвет: {color}");
+        Console.WriteLine("Введите количество элементов массива");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Введите элементы массива");
+        for (int i = 0; i < n; i++) 
+        { Console.Write($"arr[{i}]=");
+            arr[i] = int.Parse(Console.ReadLine());
         }
+        SortArray(arr);
+        Console.WriteLine(string.Join(", ", arr));
     }
-
-    static (string name, int age) Profile()
+    static void SortArray(int[] arr)
     {
-        Console.Write("Введите имя: ");
-        string name = Console.ReadLine();
-        Console.Write("Введите возраст с цифрами: ");
-        int age = Convert.ToInt32(Console.ReadLine());
-        var user = (name, age);
-        return user;
-    }
-    static string[] ShowColor(string name)
-    {
+        Array.Sort(arr);
         
-        string[] colors = new string[3];
-
-        int i = 0;
-        while (i < colors.Length)
-        {
-            Console.Write($"{name} Введите любимый цвет #{i + 1}: ");
-            colors[i] = Console.ReadLine();
-            i++;
-        }
-        return colors;
     }
 
+
+        
     
 }
